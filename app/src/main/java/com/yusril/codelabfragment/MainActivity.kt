c66f66aae1cba377c8ed8a4a239c7bedb713d28a
yusrilihzaM <mohyusrilihzamaulana@gmail.com>
2021-03-11 19:33:07 +0700

    Initial commit

---
+++ app/src/main/java/com/yusril/codelabfragment/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.yusril.codelabfragment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlin.math.log
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val mfragmentManager=supportFragmentManager
+        val mHomeFragment=HomeFragment()// buat objek dari home fragment
+        val fragment=mfragmentManager.findFragmentByTag(HomeFragment::class.java.simpleName)
+        if(fragment!is HomeFragment){
+            Log.d("My Fragment", "Fragment name"+HomeFragment::class.java.simpleName)
+
+            mfragmentManager
+                .beginTransaction()// mulai
+                    //fragment manager masukan ke id frame container, yang dimasukan objek dr homefragment, class dari Homefragment
+                .add(R.id.frame_container,mHomeFragment,HomeFragment::class.java.simpleName)
+                .commit()
+        }
+    }
+}
