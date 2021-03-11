bf68039cf8db53228267319bbd1eadc00177bd12
yusrilihzaM <mohyusrilihzamaulana@gmail.com>
2021-03-11 19:44:59 +0700

    Flexibel Fragment

--- app/src/main/java/com/yusril/codelabfragment/HomeFragment.kt
+++ app/src/main/java/com/yusril/codelabfragment/HomeFragment.kt
@@ -11,14 +11,17 @@ import android.widget.Button
 class HomeFragment : Fragment(), View.OnClickListener {
 
 
-
+    // hapus semua kecuali onCreate View kemudian buat onViewCrate
     override fun onCreateView(
+
+//Pada HomeFragment terdapat metode onCreateView() di mana layout interface didefinisikan dan ditransformasikan dari layout berupa file xml ke dalam objek view dengan menggunakan metode inflate().
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -27,7 +30,20 @@ class HomeFragment : Fragment(), View.OnClickListener {
     }
 
     override fun onClick(v: View?) {
-        TODO("Not yet implemented")
+        if (v != null) {
+            if (v.id == R.id.btn_category) {
+                val mCategoryFragment = CategoryFragment()
+                val mFragmentManager = fragmentManager
+                mFragmentManager?.beginTransaction()?.apply {
+                    replace(// replace untuk menempelkan sebuah fragment baru
+                        R.id.frame_container,
+                        mCategoryFragment,
+                        CategoryFragment::class.java.simpleName
+                    )
+                    addToBackStack(null)
+                    commit()
+                }
+            }
+        }
     }
-
 }
